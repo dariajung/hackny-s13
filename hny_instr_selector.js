cc3ec8e894f2d847b27febb7cb706a24bda9b2a8
Rachel <rs3092@barnard.edu>
2013-04-07 01:33:05 -0400

    instrument framework

---
+++ hny_instr_selector.js
@@ -0,0 +1,9 @@
+var major_scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
+var minor_scale = [261.63, 293.66, 311.13, 349.23, 392.00, 415.30, 493.88, 523.25];
+//var pent_scale = [];
+//var oct_scale = [];
+//var wt_scale = [];
+
+
+function setInstr(num){
+
